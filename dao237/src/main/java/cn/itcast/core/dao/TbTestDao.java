2e8db1560ff6141229692bd0f91735959d9d24a2
jkq <1580202279@qq.com>
2019-05-23 21:25:00 +0800

    init

---
+++ dao237/src/main/java/cn/itcast/core/dao/TbTestDao.java
@@ -0,0 +1,5 @@
+package cn.itcast.core.dao;
+
+public interface TbTestDao {
+
+}
---
+++ pojo237/src/main/java/pojo237/TbTest.java
@@ -0,0 +1,12 @@
+package pojo237;
+
+import java.util.Date;
+
+public class TbTest {
+
+
+		private Integer id;
+		private String name;
+		private Date birthday;
+
+}
